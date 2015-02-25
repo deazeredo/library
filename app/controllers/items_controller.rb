ee225f9f9f21ceca62fa65c8758ae463e48e6a00
deazeredo <deazeredo.lucas@gmail.com>
2015-02-25 08:53:12 +0000

    Added item editing views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,45 @@ class ItemsController < ApplicationController
   def index
   	@items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+   def create
+    @item = Item.new(entry_params)
+    if @item.save
+      redirect_to entries_path
+    else
+      render :new
+    end
+  end
+
+    def update
+    @item = Item.find(params[:id])
+
+    if @item.update_attributes(entry_params)
+      redirect_to entries_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to entries_path
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to entries_path
+  end
+
+
 end
