0f8bc68e07159b1ffc30dc2dedeb3c019a83d055
deazeredo <deazeredo.lucas@gmail.com>
2015-02-25 09:30:54 +0000

    Wired up ImageFetcher

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,12 @@
 class ItemsController < ApplicationController
+  
   def index
   	@items = Item.all
   end
 
   def new
     @item = Item.new
+    #@imageFetcher = ImageFetcher.new
   end
 
   def edit
@@ -12,9 +14,11 @@ class ItemsController < ApplicationController
   end
 
    def create
-    @item = Item.new(entry_params)
+    @item = Item.new(item_params)
+    #@item.image_url = ImageFetcher.new.fetch(@item.title)
+
     if @item.save
-      redirect_to entries_path
+      redirect_to items_path
     else
       render :new
     end
@@ -23,8 +27,8 @@ class ItemsController < ApplicationController
     def update
     @item = Item.find(params[:id])
 
-    if @item.update_attributes(entry_params)
-      redirect_to entries_path
+    if @item.update_attributes(item_params)
+      redirect_to items_path
     else
       render :edit
     end
@@ -33,14 +37,12 @@ class ItemsController < ApplicationController
   def destroy
     @item = Item.find(params[:id])
     @item.destroy
-    redirect_to entries_path
+    redirect_to items_path
   end
 
-  def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
-    redirect_to entries_path
+  
+ def item_params
+    params.require(:item).permit(:title, :completed_on, :item_type_id)
   end
-
 
 end
